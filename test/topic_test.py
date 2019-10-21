3eb8e33bd0e97b937d71a33655512c1090cedc89
beezee <brian.zeligson@gmail.com>
2019-10-21 12:59:18 -0400

    clean up unused typevar

--- test/topic_test.py
+++ test/topic_test.py
@@ -7,7 +7,6 @@ from typing import Callable, TypeVar
 
 A = TypeVar('A')
 B = TypeVar('B')
-C = TypeVar('C')
 
 def fibrecheck(ab: Callable[[A], B], ba: Callable[[B], A], a: A) -> None:
   res = ab(a)
