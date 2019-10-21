eae9350ab92b0ab0d0cb1ea5bb78d3eb7e1f63d4
beezee <brian.zeligson@gmail.com>
2019-10-21 12:54:22 -0400

    pulling out fibrecheck

--- test/topic_test.py
+++ test/topic_test.py
@@ -1,13 +1,26 @@
-from adt import F2, map2
+from adt import F2, lift2, liftBind2
 from hypothesis import given 
 from hypothesis.strategies import text, integers
 import json
-from topic import Foo, FooJson
+from topic import Foo, FooJson, idr
+from typing import Callable, TypeVar
+
+A = TypeVar('A')
+B = TypeVar('B')
+C = TypeVar('C')
+
+def fibrecheck(ab: Callable[[A], B], ba: Callable[[B], A], a: A) -> None:
+  res = ab(a)
+  assert ab(ba(res)) == res
 
 @given(text(), integers()) # type: ignore
 def test_foo_serialize_round_trip(s: str, i: int) -> None:
   sz = FooJson()
-  assert sz.deserialize(sz.serialize(Foo(s, i))) == F2(Foo(s, i))
+  fibrecheck(lift2(Exception, sz.serialize), 
+             liftBind2(Exception, sz.deserialize), 
+             idr(Foo)(Foo(s, i)))
   js = json.dumps({"bar": s, "baz": i}, # type: ignore
     separators=(',', ':')) # type: ignore
-  assert map2(sz.deserialize(js), sz.serialize) == F2(js)
+  fibrecheck(liftBind2(Exception, sz.deserialize),
+             lift2(Exception, sz.serialize),
+             idr(str)(js))
